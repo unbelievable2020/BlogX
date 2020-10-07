3e970c0a5d4144da20730925f19750a80a67649e
xzq <xzq2020@stu.pku.edu.cn>
2020-10-07 09:39:13 +0800

    add pom

---
+++ src/main/java/com/xzq/blogx/BlogxApplication.java
@@ -0,0 +1,13 @@
+package com.xzq.blogx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogxApplication.class, args);
+	}
+
+}
